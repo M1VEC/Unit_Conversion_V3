22d072928a9b599ec1d6098fba77d4683d1ded42
Philip Bilton <pbilton85@gmail.com>
2020-08-26 12:09:29 +0100

    add time ratios and test ratios

--- src/com/pbilton/test/com/pbilton/unit_conversion_V3/testTime.java
+++ src/com/pbilton/test/com/pbilton/unit_conversion_V3/testTime.java
@@ -1,4 +1,20 @@
 package com.pbilton.unit_conversion_V3;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static com.pbilton.unit_conversion_V3.Distance.*;
+
 public class testTime {
+
+    @Test
+    public void testRatios() {
+        Assertions.assertEquals(0.001D, Time.Millisecond.getRatio());
+        Assertions.assertEquals(1D, Time.Second.getRatio());
+        Assertions.assertEquals(60D, Time.Minute.getRatio());
+        Assertions.assertEquals(3600D, Time.Hour.getRatio());
+        Assertions.assertEquals(86400D, Time.Day.getRatio());
+        Assertions.assertEquals(31536000D, Time.Year.getRatio());
+
+    }
 }
--- src/com/pbilton/unit_conversion_V3/Time.java
+++ src/com/pbilton/unit_conversion_V3/Time.java
@@ -7,8 +7,17 @@ public class Time {
     private double ValueInSeconds;
     private double Value;
 
-    public static Time Seconds = new Time(1D,1D,"secs","Seconds");
-    
+    public static Time Millisecond = new Time(1D, 0.001D,"ms","Milliseconds");
+    public static Time Second = new Time(1D,1D,"s","Seconds");
+    public static Time Minute = new Time(1D, 60D, "m", "Minute");
+    public static Time Hour = new Time(1D,60D* Minute.Ratio,"hr","Hour");
+    public static Time Day = new Time(1D,24D* Hour.Ratio,"d","Day");
+    public static Time Year = new Time(1D,365D* Day.Ratio,"y","Year");
+
+    public static Time Create(double value, Time  unit){
+        return new Time(value, unit.Ratio, unit.ShortName, unit.LongName);
+    }
+
     public Time (double value, double ratio, String shortName, String longName){
         Value = value;
         Ratio = ratio;
@@ -17,4 +26,12 @@ public class Time {
         ValueInSeconds = Value * Ratio;
     }
 
+    public Time convertTo(Time target){
+        var targetTime = ValueInSeconds / target.Ratio;
+        return Create(targetTime, target);
+    }
+
+    public double getRatio(){
+        return this.Ratio;
+    }
 }
