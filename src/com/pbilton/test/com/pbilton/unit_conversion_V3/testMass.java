b86bf1466742bf9a46376e305e1de44f4bc08ae6
Philip Bilton <pbilton85@gmail.com>
2020-08-26 16:32:44 +0100

    Mass and testMass complete

--- src/com/pbilton/test/com/pbilton/unit_conversion_V3/testMass.java
+++ src/com/pbilton/test/com/pbilton/unit_conversion_V3/testMass.java
@@ -14,4 +14,24 @@ public class testMass {
         Assertions.assertEquals(1016046.92D, Mass.Ton.getRatio());
         Assertions.assertEquals(6350.293D, Mass.Stone.getRatio());
     }
+
+    @Test
+    public void test_tryParse_Gram_Kilogram(){
+        var baseUnit = Mass.tryParse("5500g");
+        var kg = baseUnit.tryParseUnit("kg");
+        Assertions.assertEquals(5.5D,kg.getValue());
+    }
+
+    @Test
+    public void test_tryParse_KG_Stone(){
+        var baseUnit = Mass.tryParse("67kg");
+        var st = baseUnit.tryParseUnit("stone");
+        Assertions.assertEquals(10.550694275051562D,st.getValue());
+    }
+
+    @Test
+    public void fail_tryParse_Test() {
+        var baseUnit = Time.tryParse("450 tons");
+        Assertions.assertNull(null,"Invalid unit input");
+    }
 }
--- src/com/pbilton/unit_conversion_V3/Mass.java
+++ src/com/pbilton/unit_conversion_V3/Mass.java
@@ -14,21 +14,89 @@ public class Mass {
     public static Mass Ton = new Mass(1D,1016.04692D* Kilogram.Ratio,"Ton","Ton");
     public static Mass Stone = new Mass(1D,6.350293D* Kilogram.Ratio,"st","Stone");
 
-
     public static Mass Create(double value, Mass unit){
         return new Mass(value, unit.Ratio, unit.ShortName, unit.LongName);
     }
 
     public Mass(double value, double ratio, String shortName, String longName){
-    Value = value;
-    Ratio = ratio;
-    ShortName = shortName;
-    LongName = longName;
-    ValueInGrams = Value * Ratio;
+        Value = value;
+        Ratio = ratio;
+        ShortName = shortName;
+        LongName = longName;
+        ValueInGrams = Value * Ratio;
+    }
+
+    public Mass convertTo(Mass target){
+        var targetMass = ValueInGrams / target.Ratio;
+        return Create(targetMass, target);
     }
 
     public double getRatio() {
         return this.Ratio;
     }
 
+    public String getShortName() {
+        return this.ShortName;
+    }
+
+    public String getLongName() {
+        return this.LongName;
+    }
+
+    public double getValueInGrams() {
+        return this.ValueInGrams;
+    }
+
+    public double getValue() {
+        return this.Value;
+    }
+
+    public static Mass AllUnits[] = new Mass[]{
+            Gram,
+            Milligram,
+            Kilogram,
+            Ton,
+            Stone
+    };
+
+    public static Mass tryParse(String input)  {
+        int massValue = Integer.parseInt(input.replaceAll("[^0-9]", ""));
+        String unitValue = input.replaceAll("[^a-z,A-Z]", "");
+
+        boolean contains = false;
+        Mass unit = null;
+
+        for (Mass AllUnit : AllUnits) {
+            if (unitValue.equalsIgnoreCase(AllUnit.LongName)|| unitValue.equalsIgnoreCase(AllUnit.ShortName)){
+                unit = AllUnit;
+                contains = true;
+                break;
+            }
+        }
+
+        if (contains == true)
+            return Create(massValue, unit);
+        else
+            return null;
+    }
+
+    public Mass tryParseUnit(String input){
+        String unitValue = input.replaceAll("[^a-z,A-Z]", "");
+
+        boolean contains = false;
+        Mass unit = null;
+
+        for (Mass AllUnit : AllUnits) {
+            if (unitValue.equalsIgnoreCase(AllUnit.LongName)|| unitValue.equalsIgnoreCase(AllUnit.ShortName)){
+                unit = AllUnit;
+                contains = true;
+                break;
+            }
+        }
+
+        if (contains == true)
+            return convertTo(unit);
+        else
+            return null;
+    }
 }
